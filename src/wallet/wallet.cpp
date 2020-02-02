07f1ffb5f0ae032e6ed9ef6055490d6ee737fce6
cryptoforge <cryptoforge.cc@gmail.com>
2020-02-02 10:20:26 -0800

    fix disable z_sendmany - RPC should not be called until witnesses are built.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1172,6 +1172,7 @@ int CWallet::VerifyAndSetInitialWitness(const CBlockIndex* pindex, bool witnessO
   int nWitnessTxIncrement = 0;
   int nWitnessTotalTxCount = mapWallet.size();
   int nMinimumHeight = pindex->nHeight;
+  bool walletHasNotes = false; //Use to enable z_sendmany when no notes are present
 
   for (std::pair<const uint256, CWalletTx>& wtxItem : mapWallet) {
     nWitnessTxIncrement += 1;
@@ -1180,6 +1181,7 @@ int CWallet::VerifyAndSetInitialWitness(const CBlockIndex* pindex, bool witnessO
       continue;
 
     if (wtxItem.second.GetDepthInMainChain() > 0) {
+      walletHasNotes = true;
       auto wtxHash = wtxItem.second.GetHash();
       int wtxHeight = mapBlockIndex[wtxItem.second.hashBlock]->nHeight;
 
@@ -1361,6 +1363,10 @@ int CWallet::VerifyAndSetInitialWitness(const CBlockIndex* pindex, bool witnessO
       }
     }
   }
+  //enable z_sendmany when the wallet has no Notes
+  if (!walletHasNotes)
+    initWitnessesBuilt = true;
+
   return nMinimumHeight;
 }
 
@@ -1372,9 +1378,6 @@ void CWallet::BuildWitnessCache(const CBlockIndex* pindex, bool witnessOnly)
   int startHeight = VerifyAndSetInitialWitness(pindex, witnessOnly) + 1;
 
   if (startHeight > pindex->nHeight || witnessOnly) {
-    //Set witnessBuilt to false to prevent zsendmany from attempting
-    //collect witnesses during sync
-    initWitnessesBuilt = false;
     return;
   }
 
